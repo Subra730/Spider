889101e8d0ea6e65eb9055d1c606b0f9bb6ac7de
Ganesh Chandran <cganesh8@gmail.com>
2020-05-19 11:49:04 +1000

    Removed the errorneos purple colour in menu

--- src/App.js
+++ src/App.js
@@ -13,29 +13,23 @@ import SignUp from "./SignUp";
 class App extends Component {
     render() {
         return (
-
             <HashRouter>
                 <div className="App">
-
                     <header className="sans-serif">
                         <div className="cover bg-left bg-center-l">
                             <div className="bg-black-80 pb5 pb6-m pb7-l">
-
                                 <nav className="dt w-100 mw8 center">
                                     <div className="dtc w2 v-mid pa3">
-                                        <a href="/" className="dib w2 h2 pa1 ba b--white-90 grow-large border-box">
-
-                                        </a>
+                                        <div className="dib w2 h2 pa1 ba b--white-90 grow-large border-box"></div>
                                     </div>
-
                                     <div className="dtc v-mid tr pa3">
-                                        <ul className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3">
-                                            <li className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3"><NavLink to="/">Home</NavLink></li>
-                                            <li className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3"><NavLink to="/Pricing">Pricing</NavLink></li>
-                                            <li className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3"><NavLink to="/Register">Register</NavLink></li>
-                                            <li className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3"><NavLink to="/TestTips">Test Tips</NavLink></li>
-                                            <li className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3 ba"><NavLink to="/SignUp">Sign Up</NavLink></li>
-                                        </ul>
+                                        <div className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3">
+                                            <NavLink className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" to="/">Home</NavLink>
+                                            <NavLink className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" to="/Pricing">Pricing</NavLink>
+                                            <NavLink className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" to="/Register">Register</NavLink>
+                                            <NavLink className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3" to="/TestTips">Test Tips</NavLink>
+                                            <NavLink className="f6 fw4 hover-white no-underline white-70 dn dib-ns pv2 ph3 ba" to="/SignUp">Sign Up</NavLink>
+                                        </div>
 
                                         <div className="content">
                                             <Route path="/" component={Home} />
@@ -46,18 +40,9 @@ class App extends Component {
                                         </div>
                                     </div>
                                 </nav>
-
-                                <div className="tc-l mt4 mt5-m mt6-l ph3">
-                                    <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">This is your super impressive headline</h1>
-                                    <h2 className="fw1 f3 white-80 mt3 mb4">Now a subheadline where explain your wonderful new startup even more</h2>
-                                    <a className="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" href="/">Call to Action</a>
-                                    <span className="dib v-mid ph3 white-70 mb3">or</span>
-                                    <a className="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3" href="/">Secondary call to action</a>
-                                </div>
                             </div>
                         </div>
                     </header>
-
                 </div>
             </HashRouter>
         );
--- src/Home.js
+++ src/Home.js
@@ -3,11 +3,18 @@ import React, { Component } from "react";
 class Home extends Component {
     render() {
         return (
+        <div>
             <div>
                 <h2>Teach Me 2 Drive</h2>
-                <p>At TM2D we make you a safer driver with our
-           comphrehensive structured driving lessons </p>
             </div>
+            <div className="tc-l mt4 mt5-m mt6-l ph3">
+                <h1 className="f2 f1-l fw2 white-90 mb0 lh-title">Developing safer drivers</h1>
+                <h2 className="fw1 f3 white-80 mt3 mb4">with our structured lessons adapted to suit different learners</h2>
+                <a className="f6 no-underline grow dib v-mid bg-blue white ba b--blue ph3 pv2 mb3" href="/">Call 1-800-999</a>
+                <span className="dib v-mid ph3 white-70 mb3">or</span>
+                <a className="f6 no-underline grow dib v-mid white ba b--white ph3 pv2 mb3" href="/">Register your details for callback</a>
+            </div>
+        </div>
         );
     }
 }
